3536b0d2c29323c2c7ab8a7faed3bed43fcebba7
Balázs <szaboblu@gmail.com>
2023-02-01 16:14:48 +0100

    setup aliases

--- webshopMobile/babel.config.js
+++ webshopMobile/babel.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src/'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components/index',
+            '@ui': './src/ui/index',
+            '@screens': './src/screens/index',
+            '@utils': './src/utils/index',
+            '@navigation': './src/navigation/index',
+            '@store': './src/store/index',
+            '@types': './src/types/index',
+          },
+        },
+      ],
+    ],
+  };
 };
